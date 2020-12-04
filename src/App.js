cdf562be7abd00c261940d135453aa6d517c2824
Ali Reza Talebipour <alireza.tp@gmail.com>
2020-12-04 21:19:38 +0330

    Add draw path support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import {useState} from 'react'
 import './App.css'
 import Map from './Map'
+import PathExporter from './PathExporter'
+
+
+const gradientInputStyle = {width: '30px'}
 
 function App() {
+  const [maxGradient, setMaxGradient] = useState(2)
+
   return (
-    <Map />
+    <div>
+      <label>
+        Max Gradient: &nbsp;
+        <input type="number" value={maxGradient} maxLength={2} style={gradientInputStyle}
+               onChange={(event) => setMaxGradient(event.target.value)} />
+      </label>
+      &nbsp;
+      <Map maxGradient={maxGradient} />
+    </div>
   );
 }
 
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, Polyline, Tooltip } from 'react-leaflet'
 import { useMapEvents } from 'react-leaflet'
 import { Icon } from 'leaflet'
 
-const pathOptions = { color: 'lime' }
+const uphillColors = ['#00FF00', '#65FF00', '#CCFF00', '#FFCC00', '#FF6600', '#FF0000']
+const downhillColors = ['#00FF00', '#00CC33', '#009966', '#006699', '#0033CC', '#0000FF']
+
+Array.prototype.last = function() {
+  return this.length === 0 ? null : this[this.length - 1]
+}
 
 function ClickHandler(props) {
   useMapEvents({
@@ -32,34 +37,148 @@ class Map extends Component {
   }
 
   addPoint(latlng) {
-    this.setState({path: this.state.path.concat(latlng)})
+    const currentPath = this.state.path;
+    if (currentPath.length > 0) {
+      const lastSegment = currentPath.last()
+      if (currentPath[0].isComplete()) {
+        const segment = new PathSegment(lastSegment.dst, lastSegment.dstElevation, latlng, this);
+
+        currentPath.push(segment)
+      } else {
+        currentPath[0].setDestination(latlng)
+      }
+    } else {
+      currentPath.push(new PathSegment(latlng, null, null, this))
+    }
+    this.setState({path: currentPath})
+  }
+
+  removeLastPoint() {
+    const currentPath = this.state.path;
+    if (currentPath.length === 1 && currentPath.last().dst !== null) {
+      currentPath.last().setDestination(null)
+    } else {
+      currentPath.pop()
+    }
+    this.setState({path: this.state.path})
   }
 
   render() {
     return (
-      <MapContainer center={[35.713,51.396]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[35.713,51.396]} zoom={15} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <ClickHandler callback={(latlng) => this.addPoint(latlng)} />
         { this.state.path.length > 0 &&
-          <Marker position={this.state.path[0]} icon={markerIcon('green')}>
-            <Popup>Source Location</Popup>
+          <Marker position={this.state.path[0].src} icon={markerIcon('green')}>
+            {!this.state.path[0].isComplete() &&
+              <Popup>Source Location
+                  <br />
+                  <input type="button" value="x" onClick={() => this.removeLastPoint()} />
+              </Popup>
+            }
           </Marker>
         }
-        { this.state.path.length > 1 &&
-          <Marker position={this.state.path[this.state.path.length - 1]} icon={markerIcon('red')}>
-            <Popup>Destination Location</Popup>
+        { this.state.path.length > 0 && this.state.path.last().isComplete() &&
+          <Marker position={this.state.path.last().dst} icon={markerIcon('red')}>
+            <Popup>Destination Location
+              <br />
+              <input type="button" value="x" onClick={() => this.removeLastPoint()} />
+            </Popup>
           </Marker>
         }
-        { this.state.path.map( (point, index , path) =>
-            index === 0 ? null : <Polyline pathOptions={pathOptions} positions={[path[index - 1], point]} />
-          )
+        { this.state.path.map(segment => !segment.isComplete() ? null :
+            (<Polyline pathOptions={segment.mapOptions()} positions={[segment.src, segment.dst]}>
+              { segment.gradient !== null &&
+              <Tooltip>Gradient: {segment.gradient.toFixed(2)}%</Tooltip>
+              }
+            </Polyline>)
+            )
         }
       </MapContainer>
     )
   }
 }
+
+
+class PathSegment {
+
+  constructor(src, srcElevation, dst, map) {
+    this.src = src
+    this.srcElevation = srcElevation
+    this.map = map
+    this.gradient = null
+    this.setDestination(dst)
+
+    if (srcElevation === null) {
+      this.fetchElevation(src, elevation => this.srcElevation = elevation)
+    }
+  }
+
+  setDestination(latlng) {
+    this.dst = latlng
+    this.dstElevation = null
+    if (latlng) {
+      this.fetchElevation(latlng, elevation => {
+        this.dstElevation = elevation
+      })
+    }
+  }
+
+  isComplete() {
+    return this.dst !== null
+  }
+
+  fetchElevation(latlng, callback) {
+    console.log("Fetching elevation of " + latlng)
+    fetch(`/v1/test-dataset?locations=${latlng.lat},${latlng.lng}`)
+      .then(
+        (result) => result.json(),
+        (error) => console.log("Fetching elevation failed: "  + error)
+      )
+      .then((result) => {
+        if (!result) {
+          console.log("Elevation result is empty")
+          return
+        }
+        const elevation = result.results[0].elevation;
+        callback(elevation)
+        console.log(`Elevation of ${latlng} set to ${elevation}`)
+        if (this.srcElevation !== null && this.dstElevation !== null) {
+          this.calculateGradient()
+        }
+        // Update react state
+        this.map.setState(this.map.state)
+      })
+  }
+
+  calculateGradient() {
+    this.distance = this.src.distanceTo(this.dst)
+    const elevationDiff = (this.dstElevation - this.srcElevation);
+    const sin = elevationDiff / this.distance
+    const cos = Math.sqrt(1 - Math.pow(sin, 2))
+    this.gradient = sin * cos * 100
+  }
+
+  mapOptions() {
+    var color = 'gray'
+    if (this.gradient) {
+      const maxGradient = this.map.props.maxGradient
+      if (Math.abs(this.gradient) > maxGradient) {
+        color = 'black'
+      }
+      const colorIndex = Math.ceil(Math.abs(this.gradient) / maxGradient * (uphillColors.length - 1))
+      color = this.gradient > 0 ? uphillColors[colorIndex] : downhillColors[colorIndex]
+    }
+    return { color: color }
+  }
+
+  toString() {
+    return `(${this.src},${this.srcElevation}) => ${this.dst},${this.dstElevation}`
+  }
+}
+
 
 export default Map
